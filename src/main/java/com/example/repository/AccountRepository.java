dcbafc8acdfb3226079e0a61c12f42a14947708d
John Neilson <neilson.john38781@gmail.com>
2024-08-17 19:16:27 +0000

    completed repofiles for now

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,18 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.entity.Account;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+
+    @Query("FROM account WHERE username = :username")
+    public Account getAccountByUsername(@Param("username") String username);
+
+    @Query("FROM account")
+    public List<Account> getAllAccounts();
 }
--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,28 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.entity.Message;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    @Query("FROM message")
+    List<Message> getAllMessages();
+
+    @Query("FROM message where messageId = :messageId")
+    Message getMessageById(@Param("messageId") int messageId);
+
+    @Query("FROM message where postedBy = :userId")
+    List<Message> getAllMessagesByUserId(@Param("userId") int userId);
+
+    @Query("delete FROM message where messageId = :messageID")
+    void deleteMessageById(@Param("messageId") int messageId);
+
+    @Query("UPDATE message SET messageText=:messageText WHERE messageId=:messageId")
+    Message updateMessage(@Param("messageId") int messageId, @Param("messageText") String messageText);
+
 }
