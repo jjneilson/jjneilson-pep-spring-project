0fa4fd58703216b1851f8962135fa6736a1722fd
John Neilson <neilson.john38781@gmail.com>
2024-08-17 19:18:38 +0000

    services completed for now

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,28 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
 public class AccountService {
+
+    private final  AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount(Account account){
+        boolean notexists=accountRepository.getAccountByUsername(account.getUsername())==null;
+        boolean notblank=account.getUsername().length()>0;
+        boolean nottooshort=account.getPassword().length()>4;
+        if (notexists && notblank && nottooshort) return this.accountRepository.save(account);
+        return null;
+    }
+
+    public Account login(Account account){
+        Account possibleaccount = this.accountRepository.getAccountByUsername(account.getUsername());
+        if (possibleaccount!=null && possibleaccount.getPassword().equals(account.getPassword()))
+            return possibleaccount;
+        return null;
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,58 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+
+@Service
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> getAllMessages(){
+        return this.messageRepository.getAllMessages();
+    }
+
+    public Message getMessageById(int messageId){
+        return this.messageRepository.getMessageById(messageId);
+    }
+
+    public Message addMessage(Message message) {
+        boolean notexists=this.messageRepository.getMessageById(message.getMessageId())==null;
+        boolean notblank=message.getMessageText().length()>0;
+        boolean nottoolong=message.getMessageText().length()<=255;
+        if (notexists && notblank && nottoolong) return this.messageRepository.save(message);
+        return null;
+    }
+
+    public Message deleteMessage(int id){
+        if(this.messageRepository.getMessageById(id)!=null){
+            Message to_return = this.messageRepository.getMessageById(id);
+            this.messageRepository.deleteMessageById(id);
+            return to_return;
+        }
+        return null;
+    }
+
+    public Message updateMessage(int id, Message message){
+        boolean notexists=this.messageRepository.getMessageById(id)!=null;
+        boolean notblank=message.getMessageText().length()>0;
+        boolean nottoolong=message.getMessageText().length()<=255;
+        if (notexists && notblank && nottoolong) {
+            messageRepository.updateMessage(id, message.getMessageText());
+            return this.messageRepository.getMessageById(id);
+        }
+        return null;
+    }
+
+    public List<Message> getAllMessagesByUserId(int userId){
+        return this.messageRepository.getAllMessagesByUserId(userId);
+    }
 }
